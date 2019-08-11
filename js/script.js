cf079200d68a32d68b6fd33c713c1af86e2433e3
Kseniia <47961615+kkuvaldina@users.noreply.github.com>
2019-08-10 20:27:18 -0600

    final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+var audio1 = new Audio('Blop-Mark_DiAngelo.mp3');
+
+function clickMenu() {
+
+audio1.play('Blop-Mark_DiAngelo.mp3');
+
+}
+
 $(document).ready(function(){
     $(".navbar a, footer a[href='#myPage'], .home-header a, .navbar-brand").on('click', function(event) {
     if (this.hash !== "") {
